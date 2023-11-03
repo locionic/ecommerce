3e30dc4c5edccd0def83029629ddebc19ad0fed3
Truong Hong Loc <hongloc2206@gmail.com>
2023-11-03 16:44:07 +0700

    update env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000"
+// axios.defaults.baseURL = "http://127.0.0.1:8000"
+axios.defaults.baseURL = "https://ecommerce-x3n5.onrender.com"
 createApp(App).use(store).use(router).mount('#app')
