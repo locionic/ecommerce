13521687c7927dd2e4b86f0b87ca0e0b0a55931c
Truong Hong Loc <hongloc2206@gmail.com>
2023-11-03 16:10:58 +0700

    update again

--- main_backend/src/settings.py
+++ main_backend/src/settings.py
@@ -181,8 +181,9 @@ class Dev(Configuration):
     # ]
     CORS_ALLOW_ALL_ORIGINS = True
     CORS_ALLOW_CREDENTIALS = True
+    CSRF_TRUSTED_ORIGINS = [r"^https://\w+\.onrender\.com$", ]
 
-    
+
     PAGINATION_PAGE_SIZE = 20
     REST_FRAMEWORK = {
         "DEFAULT_AUTHENTICATION_CLASSES": [
