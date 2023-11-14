0d1ffd4580868afc4e1696c0e5ac8bb030929a2d
Loc Hong <noone000a@gmail.com>
2023-11-14 17:59:54 +0700

    change staticurl

--- main_backend/src/settings.py
+++ main_backend/src/settings.py
@@ -130,8 +130,10 @@ class Dev(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-    STATIC_URL = 'static/'
-    STATIC_ROOT = 'static/'
+    # STATIC_URL = 'static/'
+    # STATIC_ROOT = 'static/'
+    STATIC_URL = 'media/'
+    STATIC_ROOT = 'media/'
     # define where uploaded files are saved.
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
     # URL/path to serve media from.
